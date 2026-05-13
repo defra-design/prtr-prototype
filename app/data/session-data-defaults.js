b711ada3901538b270d4b49f0f2e9c274394ca2e
rachel-robson-defra <rachel.robson@defra.gov.uk>
2026-05-13 09:57:58 +0100

    progressing PRTR initial designs

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -1,5 +1,103 @@
 module.exports = {
 
-  // Insert values here
+    // PUBLIC REGISTER
+
+  //Search results basic + location
+  "results": [
+    {
+      "facilityName" : "Brunswick Waste Reception Site",
+      "distance" : "0.1 miles away",
+      "activity" : "Disposal of non-hazardous waste",
+      "reported" : [
+      "Pollutant releases (2024)",
+      "Pollutant transfers (2024)",
+        "Waste transfers (2024)"
+      ],
+      "actionLink" : "facility-record"
+    },
+    {
+      "facilityName" : "Byker Reclamation Plant",
+      "distance" : "0.8 miles away",
+      "activity" : "Hazardous waste",
+      "reported" : [
+      "Waste transfers (2023)"
+      ],
+      "actionLink" : ""
+    },
+    {
+      "facilityName" : "Byker Waste Reception Site",
+      "distance" : "0.8 miles away",
+      "activity" : "Disposal of non-hazardous waste",
+      "reported" : [
+      "Waste transfers (2021)"
+      ],
+      "actionLink" : ""
+    },
+    {
+      "facilityName" : "Cityworks - Newington Road",
+      "distance" : "2 miles away",
+      "activity" : "Disposal of non-hazardous waste",
+      "reported" : [
+      "Pollutant transfers (2024)"
+      ],
+      "actionLink" : ""
+    },
+    {
+      "facilityName" : "CJM Recycling Limited (Newcastle)",
+      "distance" : "2.8 miles away",
+      "activity" : "Disposal of non-hazardous waste",
+      "reported" : [
+      "Waste transfers (2024)"
+      ],
+      "actionLink" : ""
+    },
+    {
+      "facilityName" : "E R M Burnopfield",
+      "distance" : "4.2 miles away",
+      "activity" : "Disposal of non-hazardous waste",
+      "reported" : [
+      "Waste transfers (2019)"
+      ],
+      "actionLink" : ""
+    },
+    {
+      "facilityName" : "Greggs Balliol Park Longbenton - EPR/GP3532PR",
+      "distance" : "5.1 miles away",
+      "activity" : "Food and Beverage products",
+      "reported" : [
+      "Pollutant releases (2024)",
+      "Pollutant transfers (2024)",
+      "Waste transfers (2024)"
+      ],
+      "actionLink" : ""
+    },
+    {
+      "facilityName" : "JMC Metal and Waste Recycling",
+      "distance" : "5.6 miles away",
+      "activity" : "Disposal of non-hazardous waste",
+      "reported" : [
+      "Waste transfers (2024)"
+      ],
+      "actionLink" : ""
+    },
+    {
+      "facilityName" : "Kirkley",
+      "distance" : "9 miles away",
+      "activity" : "Disposal of non-hazardous waste",
+      "reported" : [
+      "No data reported"
+      ],
+      "actionLink" : ""
+    },
+    {
+      "facilityName" : "Lintz Hall Layer Unit - EPR/CP3337MA",
+      "distance" : "10.4 miles away",
+      "activity" : "Intensive livestock production",
+      "reported" : [
+      "Pollutant releases (2024)"
+      ],
+      "actionLink" : ""
+    }
+  ]
 
 }
--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,18 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+
+// Search type redirect - initial design
+router.post('/search-type', function(request, response) {
+
+	var searchselection = request.session.data['searchFacility']
+	if (searchselection == "location"){
+		response.redirect("public/location-search.html")
+	} else if (searchselection == "facility"){
+		response.redirect("public/facility-search.html")
+	} else if (searchselection == "region"){
+		response.redirect("public/region-search.html")
+	} else if (searchselection == "river"){
+		response.redirect("public/river-basin-search.html")
+	}
+})
